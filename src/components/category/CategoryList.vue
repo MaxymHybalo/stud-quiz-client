138da589f20ffeb8572f0b2b7581dc812c63de5e
esdth <keepass.log@gmail.com>
2017-03-20 21:04:34 +0200

    Added CategoryView server error handler

--- src/components/category/CategoryList.vue
+++ src/components/category/CategoryList.vue
@@ -1,10 +1,12 @@
 <template id="category-list">
     <div class="list-group">
+        <error-msg-component v-if="isError" v-bind:msg="errorMsg"/>
         <a class="list-group-item router-link-active" v-for="item in subjects" v-bind:href="'#/category/'+item.id">{{item.name}}</a>
-</div>
+    </div>
 </template>
 <script>
     import axios from 'axios'
+    import ErrorMsgComponent from '../support/ErrorMsgComponent.vue'
     export default {
         name: 'category-list',
         data: function(){
@@ -15,15 +17,15 @@
             }
         },
         created: function(){
-            // if(this.$router.params != null){
-            //     console.log("URL param: " + this.$router.params.id);
-            // }
             console.log(this.$router);
             this.subjects = axios.get('/category/names')
                                 .then(response => this.subjects = response.data)
                                 .catch(error => {
                                     this.errorMsg = 'Помилка зв\'язку з серверомм';
                                     this.isError = true;});
+        },
+        components:{
+            ErrorMsgComponent
         }
 
     }
---
+++ src/components/support/ErrorMsgComponent.vue
@@ -0,0 +1,9 @@
+<template id="error-msg-component">
+    <div class="alert alert-danger" role="alert" style="text-align: center"><strong>{{msg}}</strong></div>
+</template>
+<script>
+    export default {
+        name:'error-msg-component',
+        props:['msg']
+    }
+</script>
