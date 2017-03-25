a647f62f5353e7a5782e04c5be155efe0b796efc
esdth <keepass.log@gmail.com>
2017-03-25 16:37:19 +0200

    Add save method

--- src/components/question/AddQuestion.vue
+++ src/components/question/AddQuestion.vue
@@ -11,7 +11,11 @@
                     <div class="col-md-10">
                         <div class="form-group">
                             <label>Умова:</label>
-                            <input type="textarea" class="form-control" placeholder="Вкажіть запитання"/>
+                            <input
+                            type="textarea"
+                            class="form-control"
+                            placeholder="Вкажіть запитання"
+                            v-model="condition"/>
                         </div>
                         <div class="form-group">
                             <label>Варіанти відповіді:</label>
@@ -23,22 +27,23 @@
                             @click="rebuild"/>
                         </div>
 
-                        <div class="form-group" v-for="item in questions">
-                            <label>Варіант відповіді {{ item }}:</label>
+                        <div class="form-group" v-for="(item, index) in questions">
+                            <label>Варіант відповіді {{ index + 1 }}:</label>
                             <input
                             type="text"
-                            placeholder="Відповідь">
+                            placeholder="Відповідь"
+                            v-model="item.answer">
                             <label>Правильна?</label>
-                            <select>
-                                <option value="false">Ні</option>
-                                <option value="true">Так</option>
+                            <select v-model="item.isTruth">
+                                <option value=false>Ні</option>
+                                <option value=true>Так</option>
                             </select>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-3 col-md-offset-9">
-                        <div class="btn btn-info">
+                        <div class="btn btn-info" @click="save">
                             Зберегти
                         </div>
                     </div>
@@ -48,22 +53,42 @@
     </div>
 </template>
 <script>
-
+    import axios from 'axios'
     export default{
         name: 'add-question',
         data: function(){
             return {
                 count: null,
-                questions: [1]
+                questions: [{}],
+                condition: null
             }
         },
         methods:{
             rebuild(){
-                console.log(this.count);
-                this.questions = new Array();
+                    this.questions = new Array();
                 for (var i = 0; i < this.count; i++) {
-                    this.questions.push(i+1);
+                    this.questions.push({});
                 }
+            },
+            save(){
+                let caseProjection = {condition: this.condition};
+                let qMap = {};
+                for (var i=0; i < this.questions.length; i++) {
+                    qMap[this.questions[i].answer] = this.questions[i].isTruth;
+                }
+                caseProjection['options'] = qMap;
+
+                let params = {};
+
+                for (var key in this.$route.query.before) {
+                    params[key] = this.$route.query.before[key];
+                }
+                params['questionCase'] = this.$route.query.case;
+
+                axios.post('/question/', {params:
+                    params, data: caseProjection});
+
+                console.log(caseProjection);
             }
         }
     }
