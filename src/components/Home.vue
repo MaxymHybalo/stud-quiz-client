409edb5ec0feec339e9c63fa08c99ed5238fb25b
esdth <keepass.log@gmail.com>
2017-04-10 16:07:18 +0300

    Home page menu rev. 1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,35 +3,56 @@
         <div class="col-md-3">
             <list-component
             title="Категорія"
-            v-bind:items="items"
-            v-bind:builder="tryEmit"
+            v-bind:items="categories"
+            v-bind:builder="categoryAction"
             :remove_item="false"
-            @ems='isEms()'/>
+            />
+        </div>
+        <div class="col-md-4" v-if="category">
+            <list-component
+            title="Предмети"
+            :builder="subjectAction"
+            v-bind:items="category.subjects"
+            :remove_item="false"
+            />
+        </div>
+        <div class="col-md-5" v-if="questions">
+            <list-component
+            title="Тести"
+            v-bind:items="questions.questionCases"
+            :remove_item="false"
+            />
         </div>
     </div>
 </template>
 <script>
     import ListComponent from './support/ListComponent.vue'
-    import {getCategoriesNames} from '../utils/queries.js'
+    import {getCategoriesNames, getQuery} from '../utils/queries.js'
     export default {
         data:function(){
                 return{
-                    items:[],
+                    categories:[],
+                    category:null,
+                    selectedCategory:null,
+                    questions:null,
                     isHomeView: true
                 }
 
         },
         methods:{
-            tryEmit(item){
-                console.log("Emitt builder!", item.name);
-                this.$emit('ems');
+            categoryAction(item){
+                console.log("Emitt category!", item.id);
+                this.category = getQuery("/category/"+item.id).then(response => this.category = response.data);
+                this.selectedCategory = item;  // mark selected category for serch question cases
+                this.questions = null; // disable previous question cases
             },
-            isEms(){
-                console.log("Function emited!");
+            subjectAction(item){
+                console.log("Emit subject " + item.name);
+                this.questions = getQuery("/subject/", {category: this.selectedCategory.id, name: item.name}).then(response => this.questions = response.data);
             }
         },
         created:function() {
-            this.items = getCategoriesNames().then(response => this.items = response.data);
+            this.categories = getQuery("/category/names").then(response => this.categories = response.data);
         },
         components:{
             ListComponent
