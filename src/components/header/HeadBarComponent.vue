4b3fe5d5906aa492043be8c9acefcc54059e9cbc
esdth <keepass.log@gmail.com>
2017-04-22 21:08:50 +0300

    Add different header state for authorized user

--- src/components/header/HeadBarComponent.vue
+++ src/components/header/HeadBarComponent.vue
@@ -4,13 +4,22 @@
             <div class='navbar-header'>
                 <router-link to="/" class="navbar-brand">StudQuiz</router-link>
             </div>
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse" v-if="$store.getters.getProfile==null">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
                         <router-link to="/login">Ввійти</router-link>
                     </li>
                     <li>
                         <router-link to="/signup">Реєстрація</router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse" v-else >
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <router-link to="/profile">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>{{$store.getters.getProfile.first}}
+                        </router-link>
                     </li>
                 </ul>
             </div>
