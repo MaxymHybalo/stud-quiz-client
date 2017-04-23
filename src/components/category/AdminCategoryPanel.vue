e977ac43fd64e833f09c8bbe222b9b250288fd54
esdth <keepass.log@gmail.com>
2017-04-23 18:46:03 +0300

    editor and category tree merged in one component

---
+++ src/components/category/AdminCategoryPanel.vue
@@ -0,0 +1,25 @@
+<template id="admin-category-panel">
+    <div class="">
+        <div class="row">
+            <div class="col-md-3 btn btn-primary">
+                <router-link to="/profile/category">Редактор</router-link>
+            </div>
+        </div>
+        <div class="row">
+            <home/>
+        </div>
+    </div>
+</template>
+<script>
+    import Home from '../Home.vue'
+    export default {
+        components:{
+            Home
+        }
+    }
+</script>
+<style media="screen">
+    div.btn-primary a {
+        color: white;
+    }
+</style>
